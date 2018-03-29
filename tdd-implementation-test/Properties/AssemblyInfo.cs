9994f890b19221288da765fa0082481b3fca4e1e
zjx-immersion <jianxin_home@163.com>
2018-03-29 23:47:34 +0800

    [Jason]initial

---
+++ tdd-implementation-test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("tdd-implementation-test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("tdd-implementation-test")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e68deb0d-38ac-4e12-870b-ab63b8ee813c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ tdd-implementation-test/WordFrequencyNumberProcessorTests.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+
+namespace tdd_implementation_test
+{
+    using tdd_implementation;
+
+    using Xunit;
+
+    public class WordFrequencyNumberProcessorTests
+    {
+        [Fact]
+        public void Should_retuen_empty_when_input_empty_string()
+        {
+            // Given
+            var inputStr = string.Empty;
+            var expectedResult = string.Empty;
+            var frequencyNumberProcessor = new WordFrequencyNumberProcessor();
+
+            // When
+            var result = frequencyNumberProcessor.caculate(inputStr);
+
+            // Then
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void Should_retuen_one1_when_input_one()
+        {
+            // Given
+            var inputStr = "one";
+            var expectedResult = "one 1";
+            var frequencyNumberProcessor = new WordFrequencyNumberProcessor();
+
+            // When
+            var result = frequencyNumberProcessor.caculate(inputStr);
+
+            // Then
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void Should_retuen_one1two1_when_input_one_two() //开始重构，抽取模式
+        {
+            // Given
+            var inputStr = "one two";
+            var expectedResult = "one 1\r\ntwo 1";
+            var frequencyNumberProcessor = new WordFrequencyNumberProcessor();
+
+            // When
+            var result = frequencyNumberProcessor.caculate(inputStr);
+
+            // Then
+            Assert.Equal(expectedResult, result);
+        }
+
+
+        [Fact]
+        public void Should_retuen_one2_when_input_one_one()
+        {
+            // Given
+            var inputStr = "one one two";
+            var expectedResult = "one 2\r\ntwo 1";
+            var frequencyNumberProcessor = new WordFrequencyNumberProcessor();
+
+            // When
+            var result = frequencyNumberProcessor.caculate(inputStr);
+
+            // Then
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void Should_retuen_two2one1_when_input_abb()
+        {
+            // Given
+            var inputStr = "one two two";
+            var expectedResult = "two 2\r\none 1";
+            var frequencyNumberProcessor = new WordFrequencyNumberProcessor();
+
+            // When
+            var result = frequencyNumberProcessor.caculate(inputStr);
+
+            // Then
+            Assert.Equal(expectedResult, result);
+        }
+
+
+    }
+}
